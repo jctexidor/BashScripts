4c8d56da84770d2567b5b2b541139d344c36c59d
José Carlos Rodríguez Texidor <jctexidor@uci.cu>
2021-01-29 17:13:11 -0500

    Update

--- snap_apps.sh
+++ snap_apps.sh
@@ -6,9 +6,9 @@ sudo snap set system proxy.https="http://127.0.0.1:3128"
 
 # sudo snap refresh
 sudo snap install code --classic
-sudo snap install atom --classic
-sudo snap install chromium
-sudo snap install sublime-text --classic
+# sudo snap install atom --classic
+# sudo snap install chromium
+# sudo snap install sublime-text --classic
 
 
 exit 0;
