6a396c747ed4a1c777db2096c74fb1b4db89130b
José Carlos Rodríguez Texidor <jctexidor@uci.cu>
2021-02-25 09:04:28 -0500

    Minor changes

--- chromium.sh
+++ chromium.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # RUN CHROMIUM WITH CNTLM AS PROXY.
-chromium --proxy-server="https=127.0.0.1:3128;http=127.0.0.1:3128"
+chromium --proxy-server="https=127.0.0.1:3128;http=127.0.0.1:3128" --proxy-bypass-list="*.uci.cu,10.0.0.0/8,192.168.0.0/16,169.254.0.0/16"
 exit 0
--- find.sh
+++ find.sh
@@ -2,5 +2,7 @@
 
 find . -type f -iname '*.mp4' -exec ffmpeg -i {} {}.mp3 \;
 find . -type f -iname '*.mp4' -exec ffmpeg -i {} -target pal-vcd {}.mpg \;
-
+gsettings set org.gnome.system.proxy mode 'manual' 
+gsettings set org.gnome.system.proxy.http host '127.0.0.1'
+gsettings set org.gnome.system.proxy.http port 3128
 exit 0;
