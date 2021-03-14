34ac09fcb31318e1288d17948ca2af68fa3bd076
José Carlos Rodríguez Texidor <jctexidor@uci.cu>
2021-03-13 23:31:41 -0500

    update

--- snap_apps.sh
+++ snap_apps.sh
@@ -5,8 +5,10 @@ sudo snap set system proxy.https="http://127.0.0.1:3128"
 
 sudo snap refresh                # Update the snap apps.
 sudo snap install code --classic # Visual Studio Code
-sudo snap install atom --classic
 sudo snap install chromium
+sudo snap install beekeeper-studio
+sudo snap install atom --classic
 sudo snap install sublime-text --classic
+sudo snap install telegram-desktop
 
 exit 0
